5b844bfdce52335f40c16086fd01b01a4a43b6bd
Calvin Xie <calvinjxie@gmail.com>
2025-03-09 22:20:52 -0500

    added stack operations/functions

--- proj1/mode.rkt
+++ proj1/mode.rkt
@@ -7,3 +7,16 @@
        [(string=? (vector-ref args 0) "-b") #f]
        [(string=? (vector-ref args 0) "--batch") #f]
        [else #t])))
+
+; toFloat function
+(define (toFloat n)
+  (real->double-flonum n))
+
+; stack operations
+(define (stack_push stackList value)
+  (cons value stackList)) ; append value ot stackList- list implementation of a stack
+
+(define (stack_pop)
+  (if (null? stack) ; check if empty list/stack
+    (list #f '()) ; if true return empty list/stack, #f indicates error/false
+    (list (car stack) (cdr stack)))); else return a list with top/first element and remaining stack
