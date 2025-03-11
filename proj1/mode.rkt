d54a26dddea0f1363779c9b8f047144d1a743be0
Calvin Xie <calvinjxie@gmail.com>
2025-03-10 19:21:39 -0500

    added compute prefix notation stuff

--- proj1/mode.rkt
+++ proj1/mode.rkt
@@ -10,42 +10,104 @@
 
 ; toFloat function
 (define (toFloat n)
-  (real->double-flonum n))
+  (real->double-flonum n));converts input n to double precision
 
 ; global def of stack
 (define stack '())
-(define history '(1 2 3 4)) ; history list
+(define history '()) ; history list
 
 ; stack operations
 (define (stack_push stackList value)
   (cons value stackList)) ; append value ot stackList- list implementation of a stack
 
-(define (stack_pop)
-  (if (null? stack) ; check if empty list/stack
+(define (stack_pop stackList)
+  (if (null? stackList) ; check if empty list/stack
     (list #f '()) ; if true return empty list/stack, #f indicates error/false
-    (list (car stack) (cdr stack)))); else return a list with top/first element and remaining stack
+    (list (car stackList) (cdr stackList)))); else return a list with top/first element and remaining stack
 
 ; function to evaluate prefix expression
 (define (apply_operation operation n1 n2)
   (case operation
     [("+") (values (+ n1 n2) #t)]
-    [("-") (values (- n1 n2) #t)]
+    [("-") (if (not n2) (values (- n1) #t) (values (- n1 n2) #t))] ; Unary if n2 is #f
     [("*") (values (* n1 n2) #t)]
     [("/") (if (zero? n2)
-               (values #f "Dividing by zero")
-               (values (quotient n1 n2) #t))]
-    [else (values #f (format "Unknown Operator: ~a" operation))]))
+               (values #f "Error: Dividing by zero") ;if zero return error msg
+               (values (quotient n1 n2) #t))] ;else do divide
+    [else (values #f (format "Error: Unknown Operator: ~a" operation))])) ;for anything else, return error msg
 
-(define (process_char character)
+;function to process single character
+(define (process_char character history)
   (cond
-    [(and (> (string-length character) 0) ; check if multiple characters ($1)
+    [(and (> (string-length character) 0)  ; check if multiple characters ($1)
           (string=? (substring character 0 1) "$")) ; check if $ is first char
-     (let* ([index_num (substring character 1)]
-            [index (string->number index_num)])
-       (if (and index (< index (length history)))
-           (list (list-ref history index) #t)
-           (list #f (format "invalid index: ~a" index))))]
+     (let* ([index_num (substring character 1)] ; extract substring after $ to get digit
+            [index (string->number index_num)]) ; convert to num
+       (if (and index (< index (length history))) ; check valid index in history list size
+           (list (list-ref (reverse history) index) #t) ; return value at index
+           (list #f (format "Error: invalid index: ~a" index))))] ; else return error message
     [(string->number character) ;convert to num to check if it is a number
-     (list (string->number character) #t)] ; return true
-    [else (list character #f)]))
-     
+     (list (string->number character) #t)] ; if successfully converts number returns number with true
+    [else (list character #f)])); else return character with false
+
+; function to evaluate prefix expression from string
+(define (compute_prefix str)
+  (let ([char_list (reverse (string-split str))]) ; split string into separate characters and reverses it for prefix order
+    (let loop ([char_list char_list] ;start looping
+               [current_stack stack]); initialize current_stack with global
+      (if (null? char_list) ; check if char_list is empty
+          (if (null? current_stack) ; check if current_stack is empty
+                (values #f "Error: Empty expression") ; return error if empty
+                (let ([result (car current_stack)])
+                  (if (null? (cdr current_stack))
+                      (values result #t (cons result history) current_stack)
+                      (values #f "Error: Invalid expression" history current_stack))))
+          (let* ([char (car char_list)] ; get first character from char_list
+                 [processed (process_char char)]; process the character
+                 [value (car  processed)]; extract values from processed result
+                 [is-number? (cadr processed)]); get boolean to see if it is number
+            (if is-number? ; check if is number from prev boolean
+                (loop (cdr char_list) (stack_push current_stack value)); if it is a number, loop through remaining characters and push values onto stack
+                ; else pop 2 numbers and run the operation
+                (let* ([pop1 (stack_pop current_stack)] ; pop first number from current stack
+                       [n1 (car pop1)] ; gets first num value
+                       [rest1 (cadr pop1)]) ; get remaining stack
+                  (if (not n1)
+                      (values #f "Error: Not enough operands" history stack)
+                      (if (member char '("-")) ; unary check
+                          (let-values ([(result success?) (apply_operation char n1 #f)])
+                            (if success?
+                                (loop (cdr char_list) (stack_push rest1 result))
+                                (values result success? history stack)))
+                          (let* ([pop2 (stack_pop rest1)] ;
+                                 [n2 (car pop2)]
+                                 [rest2 (cadr pop2)])
+                            (if (not n2)
+                                (values #f "Error: Not enough operands" history stack)
+                                (let-values ([(result success?) (apply_operation char n1 n2)])
+                                  (if success?
+                                      (loop (cdr char_list) (stack_push rest2 result))
+                                      (values result success? history stack))))))))))))))
+                      
+(define (interactive_mode)
+  (displayln "Prefix Calculator: Enter a prefix expression ( + 1 2) or 'quit' to stop")
+  (let loop()
+    (display "> ")
+    (let ([input (read-line)])
+      (unless (string=? input "quit")
+        (let-values ([(result success?) (compute_prefix input)])
+          (if success?
+              (printf "~a: ~a\nHistory: ~a\n" (length history) result (reverse history))
+              (printf "Error: ~a\n" result)))
+        (loop))))
+  (displayln "exiting Calculator."))
+
+(define (batch_mode)
+  (let-values ([(result success?) (compute_prefix (read-line))])
+    (if success?
+        (displayln (toFloat result))
+        (displayln (string-append "Error: " result)))))
+
+(if prompt?
+  (interactive_mode)
+  (batch_mode))
